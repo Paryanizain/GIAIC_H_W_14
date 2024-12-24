e379fb62093b748857d3e0ef9ccaf020aa2aedd2
zain <paryanizain@gmail.com>
2024-12-24 18:02:19 +0500

    2nd commit

--- class-15-hm/my-app/app/page.tsx
+++ class-15-hm/my-app/app/page.tsx
@@ -3,12 +3,18 @@ import Link from "next/link";
 export default async function Home() {
   const url = await fetch("https://jsonplaceholder.typicode.com/todos");
   const res = await url.json();
+
   return (
     <div>
       <h1 className="text-3xl text-center font-bold">Using JSON Placeholder API</h1>
       {res.map((todo: any) => (
-        <Link href={`${todo.id}`}>
-        <div>{todo.title}&nbsp;&nbsp;{todo.id}<br/><br/><br/></div>
+        <Link key={todo.id} href={`${todo.id}`}>
+          <div>
+            {todo.title}&nbsp;&nbsp;{todo.id}
+            <br />
+            <br />
+            <br />
+          </div>
         </Link>
       ))}
     </div>
